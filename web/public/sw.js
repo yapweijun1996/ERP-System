c78f290c835ac38c496adbf6e7579bd35ae9c0b9
video1526333 <yapweijun1996@gmail.com>
2026-09-14 22:46:51 +0800

    Bump PWA cache for provider test UI

--- web/public/sw.js
+++ web/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'erp-system-pwa-v268';
+const CACHE_VERSION = 'erp-system-pwa-v269';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
