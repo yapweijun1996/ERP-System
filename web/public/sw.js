fd47950f8f52d9b60201f061395076e68614bc2b
video1526333 <yapweijun1996@gmail.com>
2026-09-16 10:42:30 +0800

    Refresh PWA assets after password bootstrap update

--- web/public/sw.js
+++ web/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'erp-system-pwa-v279';
+const CACHE_VERSION = 'erp-system-pwa-v280';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
