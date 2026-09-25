61240fec43c2aa58f9750ff8c666f5ec07fc9c4c
video1526333 <yapweijun1996@gmail.com>
2026-09-25 23:44:20 +0800

    Fix password input seam beside visibility toggle

--- web/public/sw.js
+++ web/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'erp-system-pwa-v292';
+const CACHE_VERSION = 'erp-system-pwa-v293';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
